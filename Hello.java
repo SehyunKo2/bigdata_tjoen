c77f44cea7b098bddeb902771751bd83672bb895
SehyunKo2 <sehyunko2@gmail.com>
2021-05-29 21:40:29 +0900

    first upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+// 한줄 주석
+
+/* 여러줄 주석 
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Hello {
+	// JVM은 main() 함수를 실행함 
+	public static void main(String[] args) {
+		System.out.println("Hello, World!"); 
+	}
+}
+
+
